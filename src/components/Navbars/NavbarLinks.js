19df38d45aa11a241995b19fc194d62f4bf613ef
SullFurix <sull.furix@gmail.com>
2021-12-25 00:51:18 +0100

    cross-chain: add cross chain switch

--- src/components/Navbars/NavbarLinks.js
+++ src/components/Navbars/NavbarLinks.js
@@ -7,13 +7,30 @@ import {
   ButtonGroup,
   Button,
   Flex,
+  Menu,
   Text,
+  MenuButton,
+  Image,
+  MenuList,
+  MenuItem,
   useColorModeValue,
 } from "@chakra-ui/react";
+
 // Custom Icons
-import { ProfileIcon, SupportIcon, ExitIcon } from "../../components/Icons/Icons";
+import {
+  ProfileIcon,
+  SupportIcon,
+  ExitIcon,
+} from "../../components/Icons/Icons";
+
 // Custom Components
 import { SidebarResponsive } from "../../components/Sidebar/Sidebar";
+/*import { AnimatedNumber } from "../../components/AnimatedNumber/AnimatedNumber";
+
+<AnimatedNumber
+  animateToNumber={parseFloat(coinBalance).toFixed(3)}
+/>*/
+
 import { CHAIN_INFO } from "../../utils/constants";
 
 import { useWeb3React } from "@web3-react/core";
@@ -50,54 +67,124 @@ export default function HeaderLinks(props) {
       flexDirection="row"
     >
       {account ? (
-        <ButtonGroup size="sm" isAttached me={{ sm: "2px", md: "16px" }}>
-          <Button>
-            <Text mt="3px">
-              {parseFloat(coinBalance).toFixed(3)}{" "}
-              {CHAIN_INFO[chainId].nativeCoin.name}
-            </Text>
-          </Button>
-          <Button>
-            <Text mt="3px">
-              {parseFloat(tokenBalance).toFixed(1)}{" "}
-              {CHAIN_INFO[chainId].mainToken.name}
-            </Text>
-          </Button>
-          {ens ? (
+        <>
+          <ButtonGroup size="sm" isAttached me={{ sm: "2px", md: "16px" }}>
             <Button>
-              <Text mt="3px">{ens}</Text>
+              <Menu>
+                <MenuButton>
+                  <Flex alignItems="center" flexDirection="row">
+                    <Image
+                      w="18px"
+                      h="18px"
+                      me="10px"
+                      src={CHAIN_INFO[chainId].logo}
+                    />
+                    <Text mt="3px" fontSize="sm" fontWeight="semibold">
+                      {CHAIN_INFO[chainId].label}
+                    </Text>
+                  </Flex>
+                </MenuButton>
+                {/*<MenuList borderRadius="20px" alignItems="center">
+                  <Flex flexDirection="column">
+                    {Object.keys(CHAIN_INFO).map((id) => {
+                      return (
+                        <MenuItem
+                          borderRadius="20px"
+                          onClick={async () => {
+                            try {
+                              // check if the chain to connect to is installed
+                              await window.ethereum.request({
+                                method: "wallet_switchEthereumChain",
+                                params: [{ chainId: "0x" + id }], // chainId must be in hexadecimal numbers
+                              });
+                            } catch (error) {
+                              // This error code indicates that the chain has not been added to MetaMask
+                              // if it is not, then install it into the user MetaMask
+                              if (error.code === 4902) {
+                                try {
+                                  await window.ethereum.request({
+                                    method: "wallet_addEthereumChain",
+                                    params: [
+                                      {
+                                        chainId: "0x" + id,
+                                        rpcUrl: CHAIN_INFO[id].rpcUrl,
+                                      },
+                                    ],
+                                  });
+                                } catch (addError) {
+                                  console.error(addError);
+                                }
+                              }
+                              console.error(error);
+                            }
+                          }}
+                        >
+                          <Image
+                            w="18px"
+                            h="18px"
+                            me="10px"
+                            src={CHAIN_INFO[id].logo}
+                          />
+                          <Text fontSize="15px">{CHAIN_INFO[id].label}</Text>
+                        </MenuItem>
+                      );
+                    })}
+                  </Flex>
+                </MenuList>*/}
+              </Menu>
             </Button>
-          ) : (
             <Button>
-              <Text mt="3px">
-                {account.substring(0, 6)}...
-                {account.substring(account.length - 4)}
-              </Text>
+              <Flex flexDirection="row" mt="3px">
+                {parseFloat(coinBalance).toFixed(3)}{" "}
+                {CHAIN_INFO[chainId].nativeCoin.symbol}
+              </Flex>
             </Button>
-          )}
+            <Button>
+              <Flex flexDirection="row" mt="3px">
+                {parseFloat(tokenBalance).toFixed(1)}{" "}
+                {CHAIN_INFO[chainId].mainToken.symbol}
+              </Flex>
+            </Button>
+            {ens ? (
+              <Button>
+                <Flex mt="3px">{ens}</Flex>
+              </Button>
+            ) : (
+              <Button>
+                <Text mt="3px">
+                  {account.substring(0, 6)}...
+                  {account.substring(account.length - 4)}
+                </Text>
+              </Button>
+            )}
 
-          {connector === "walletconnect" ? (
-            <Button
-              size="sm"
-              variant="solid"
-              onClick={() => {
-                connector.close();
-              }}
-            >
-              <ExitIcon />
-            </Button>
-          ) : (
-            <Button
-              size="sm"
-              variant="solid"
-              onClick={() => {
-                deactivate();
-              }}
-            >
-              <ExitIcon />
-            </Button>
-          )}
-        </ButtonGroup>
+            {connector === "walletconnect" ? (
+              <Button
+                size="sm"
+                variant="solid"
+                onClick={() => {
+                  connector.close();
+                }}
+              >
+                <Flex mt="3px">
+                  <ExitIcon />
+                </Flex>
+              </Button>
+            ) : (
+              <Button
+                size="sm"
+                variant="solid"
+                onClick={() => {
+                  deactivate();
+                }}
+              >
+                <Flex mt="3px">
+                  <ExitIcon />
+                </Flex>
+              </Button>
+            )}
+          </ButtonGroup>
+        </>
       ) : (
         <NavLink to="user/connect">
           <Button
