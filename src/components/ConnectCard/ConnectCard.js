193600ebbbff6e09c993fbee6147c7e06f3505ca
SullFurix <sull.furix@gmail.com>
2021-12-28 14:20:36 +0100

    components: change button disign

--- src/components/ConnectCard/ConnectCard.js
+++ src/components/ConnectCard/ConnectCard.js
@@ -30,14 +30,17 @@ function ConnectCard(props) {
             fontSize="xs"
             variant="no-hover"
             px="75px"
-            bg="radial-gradient( circle at 100% 100%, transparent 9px, #7f3bd4 9px, #7f3bd4 11px, transparent 11px ), linear-gradient(to right, #7f3bd4, #fe1ae7), radial-gradient( circle at 0% 100%, transparent 9px, #fe1ae7 9px, #fe1ae7 11px, transparent 11px ), linear-gradient(to bottom, #bf31ff, #bf31ff), radial-gradient( circle at 0% 0%, transparent 9px, #fe1ae7 9px, #fe1ae7 11px, transparent 11px ), linear-gradient(to left, #fe1ae7, #7f3bd4), radial-gradient( circle at 100% 0%, transparent 9px, #7f3bd4 9px, #7f3bd4 11px, transparent 11px ), linear-gradient(to top, #7f3bd4, #7f3bd4)"
-            bgSize="11px 11px, calc(100% - 22px) 2px, 11px 11px, 2px calc(100% - 22px)"
-            bgRepeat="no-repeat"
-            bgPosition="top left, top center, top right, center right, bottom right, bottom center, bottom left, center left"
-            borderRadius="0px"
+            bg="linear-gradient(73.05deg, #fe1ae7 0%, #7f3bd5 100%)"
+            borderRadius="11px"
             _hover={{
-              bg: "linear-gradient(73.05deg, #fe1ae7 0%, #7f3bd5 100%)",
-              borderRadius: "11px",
+              bg:
+                "radial-gradient( circle at 100% 100%, transparent 9px, #7f3bd4 9px, #7f3bd4 11px, transparent 11px ), linear-gradient(to right, #7f3bd4, #fe1ae7), radial-gradient( circle at 0% 100%, transparent 9px, #fe1ae7 9px, #fe1ae7 11px, transparent 11px ), linear-gradient(to bottom, #bf31ff, #bf31ff), radial-gradient( circle at 0% 0%, transparent 9px, #fe1ae7 9px, #fe1ae7 11px, transparent 11px ), linear-gradient(to left, #fe1ae7, #7f3bd4), radial-gradient( circle at 100% 0%, transparent 9px, #7f3bd4 9px, #7f3bd4 11px, transparent 11px ), linear-gradient(to top, #7f3bd4, #7f3bd4)",
+              bgSize:
+                "11px 11px, calc(100% - 22px) 2px, 11px 11px, 2px calc(100% - 22px)",
+              bgRepeat: "no-repeat",
+              bgPosition:
+                "top left, top center, top right, center right, bottom right, bottom center, bottom left, center left",
+              borderRadius: "0px",
             }}
           >
             Connect
