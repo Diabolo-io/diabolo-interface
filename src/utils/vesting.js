e4547757bf65dd55340a19a454cc0475348ed1ea
SullFurix <sull.furix@gmail.com>
2021-12-30 02:43:07 +0100

    utils: update with new smart contract, add chart value

--- src/utils/vesting.js
+++ src/utils/vesting.js
@@ -2,12 +2,11 @@ import { useState, useEffect } from "react";
 
 import { useWeb3React } from "@web3-react/core";
 import { useUpdater } from "./updater";
-
+import { fromWeiWithDecimals } from "./decimals";
 import { ethers } from "ethers";
 
 import { CHAIN_INFO } from "./constants";
 
-/*todo update with new smart contract*/
 export function useVesting() {
   const [vesting, setVesting] = useState(undefined);
 
@@ -19,6 +18,7 @@ export function useVesting() {
     if (library && chainId && account && CHAIN_INFO[chainId].vesting) {
       let vestingInfos = {};
       let contract,
+        tokenContract,
         claimableAmounts,
         claimedAmounts,
         lockedAmounts,
@@ -28,11 +28,26 @@ export function useVesting() {
         name,
         symbol,
         token,
-        decimal;
+        decimals;
 
-      let totalClaimedAmounts,
-        totalClaimableAmounts,
+      let totalClaimedAmounts = 0,
+        totalClaimableAmounts = 0,
         totalLockedAmounts = 0;
+
+      let chartId = 0;
+      let chartData = [];
+      let dashArray = [];
+      let chartValues = [];
+
+      let colors = [
+        "#7f3bd5",
+        "#fe1ae7",
+        "#400091",
+        "#fb80ff",
+        "#aa00ff",
+        "#ff00ea",
+      ];
+      let colorId = 0;
 
       for (const lockup in CHAIN_INFO[chainId].vesting) {
         contract = new ethers.Contract(
@@ -42,31 +57,80 @@ export function useVesting() {
         );
 
         if (await contract.lockedAddressExist(account)) {
-          claimedAmounts = await contract.claimedAmounts(account);
+          token = await contract.token();
+
+          tokenContract = new ethers.Contract(
+            token,
+            CHAIN_INFO[chainId].vesting[lockup].tokenAbi,
+            library
+          );
+
+          decimals = await tokenContract.decimals();
+
+          symbol = await tokenContract.symbol();
+
+          claimedAmounts = fromWeiWithDecimals(
+            await contract.claimedAmounts(account),
+            decimals
+          );
 
           totalClaimedAmounts += claimedAmounts;
 
-          claimableAmounts = await contract.claimableAmounts(account);
+          claimableAmounts = fromWeiWithDecimals(
+            await contract.claimableAmounts(account),
+            decimals
+          );
 
           totalClaimableAmounts += claimableAmounts;
 
-          lockedAmounts = await contract.lockedAmounts(account);
+          lockedAmounts = fromWeiWithDecimals(
+            await contract.lockedAmounts(account),
+            decimals
+          );
 
           totalLockedAmounts += lockedAmounts;
 
-          unlockBegin = await contract.unlockBegin();
+          unlockBegin = (await contract.unlockBegin()).toNumber();
 
-          unlockCliff = await contract.unlockCliff();
+          unlockCliff = (await contract.unlockCliff()).toNumber();
 
-          unlockEnd = await contract.unlockEnd();
+          unlockEnd = (await contract.unlockEnd()).toNumber();
 
           name = await contract.name();
 
-          token = await contract.token();
+          if (unlockBegin < unlockCliff) {
+            chartData[chartId] = {
+              name: name,
+              data: [
+                [unlockBegin * 1000, 0],
+                [
+                  unlockCliff * 1000,
+                  (lockedAmounts * (unlockCliff - unlockBegin)) /
+                    (unlockEnd - unlockBegin),
+                ],
+              ],
+            };
+            dashArray.push(5);
+            chartId++;
+          }
 
-          /*  decimal = await erc20.;
+          let interval = (unlockEnd - unlockCliff) / 10;
 
-          symbol = await erc20. */
+          for (let i = unlockCliff; i <= unlockEnd; i += interval) {
+            chartValues.push([
+              i * 1000,
+              (lockedAmounts * (i - unlockBegin)) / (unlockEnd - unlockBegin),
+            ]);
+          }
+
+          chartData[chartId] = {
+            name: name,
+            data: chartValues,
+          };
+          chartId += 1;
+          dashArray.push(0);
+
+          chartValues = [];
 
           vestingInfos[lockup] = {
             claimedAmounts: claimedAmounts,
@@ -78,16 +142,89 @@ export function useVesting() {
             address: CHAIN_INFO[chainId].vesting[lockup].address,
             name: name,
             symbol: symbol,
+            decimals: decimals,
+            color: colors[colorId],
           };
-          vestingInfos["total"] = {
-            totalClaimedAmounts: totalClaimedAmounts,
-            totalClaimableAmounts: totalClaimableAmounts,
-            totalLockedAmounts: totalLockedAmounts,
-          };
-          setVesting(vestingInfos);
+          colorId += 1;
         } else {
           setVesting(false);
         }
+      }
+
+      const chartOptions = {
+        chart: {
+          zoom: {
+            enabled: false,
+          },
+          toolbar: {
+            show: false,
+          },
+        },
+        tooltip: {
+          theme: "dark",
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          curve: "straight",
+          dashArray: dashArray,
+        },
+        xaxis: {
+          type: "datetime",
+          colors: colors,
+          labels: {
+            style: {
+              colors: "#c8cfca",
+              fontSize: "12px",
+            },
+          },
+        },
+        yaxis: {
+          labels: {
+            style: {
+              colors: "#c8cfca",
+              fontSize: "12px",
+            },
+          },
+        },
+        legend: {
+          show: false,
+        },
+        grid: {
+          strokeDashArray: 5,
+        },
+        fill: {
+          type: "gradient",
+          gradient: {
+            shade: "light",
+            type: "vertical",
+            shadeIntensity: 0.5,
+            gradientToColors: undefined, // optional, if not defined - uses the shades of same color in series
+            inverseColors: true,
+            opacityFrom: 0.8,
+            opacityTo: 0,
+            stops: [],
+          },
+          colors: colors,
+        },
+        colors: colors,
+      };
+
+      vestingInfos["total"] = {
+        name: "total",
+        totalClaimedAmounts: totalClaimedAmounts,
+        totalClaimableAmounts: totalClaimableAmounts,
+        totalLockedAmounts: totalLockedAmounts,
+        symbol: symbol,
+        chartOptions: chartOptions,
+        chartData: chartData,
+      };
+
+      if (Object.keys(vestingInfos).length) {
+        setVesting(vestingInfos);
+      } else {
+        setVesting(false);
       }
     } else {
       setVesting(false);
