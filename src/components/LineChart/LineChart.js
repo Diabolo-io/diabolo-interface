004c4fa45bd5a848b29e2fc8e48358cec3b34fb2
SullFurix <sull.furix@gmail.com>
2021-12-30 02:39:31 +0100

    components: update LineChart

--- src/components/LineChart/LineChart.js
+++ src/components/LineChart/LineChart.js
@@ -1,60 +1,7 @@
 import ReactApexChart from "react-apexcharts";
 
 export default function ApexChart(props) {
-  const options = {
-    chart: {
-      toolbar: {
-        show: false,
-      },
-    },
-    tooltip: {
-      theme: "dark",
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      curve: "smooth",
-    },
-    xaxis: {
-      categories: props.chartOptions.categories,
-      labels: {
-        style: {
-          colors: "#c8cfca",
-          fontSize: "12px",
-        },
-      },
-    },
-    yaxis: {
-      labels: {
-        style: {
-          colors: "#c8cfca",
-          fontSize: "12px",
-        },
-      },
-    },
-    legend: {
-      show: false,
-    },
-    grid: {
-      strokeDashArray: 5,
-    },
-    fill: {
-      type: "gradient",
-      gradient: {
-        shade: "light",
-        type: "vertical",
-        shadeIntensity: 0.5,
-        gradientToColors: undefined, // optional, if not defined - uses the shades of same color in series
-        inverseColors: true,
-        opacityFrom: 0.8,
-        opacityTo: 0,
-        stops: [],
-      },
-      colors: props.chartOptions.colors,
-    },
-    colors: props.chartOptions.colors,
-  };
+  const options = props.chartOptions;
 
   const data = props.chartData;
 
