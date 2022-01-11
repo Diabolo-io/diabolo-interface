9f39aa0be11949cf0981853463b65ffaf4369507
SullFurix <sull.furix@gmail.com>
2022-01-11 22:46:17 +0100

    connectors: remove ledger

--- src/utils/connectors.js
+++ src/utils/connectors.js
@@ -1,6 +1,6 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
-import { LedgerConnector } from "@web3-react/ledger-connector";
+//import { LedgerConnector } from "@web3-react/ledger-connector";
 //import { TrezorConnector } from "@web3-react/trezor-connector";
 
 import { CHAIN_INFO } from "./constants";
@@ -18,13 +18,13 @@ export const walletconnect = new WalletConnectConnector({
   qrcode: true,
   pollingInterval: POLLING_INTERVAL,
 });
-
+/*
 export const ledger = new LedgerConnector({
   chainId: 1,
   url: CHAIN_INFO[1].infura,
   pollingInterval: POLLING_INTERVAL,
 });
-/*
+
 export const trezor = new TrezorConnector({
   chainId: 1,
   url: RPC_URLS[1],
