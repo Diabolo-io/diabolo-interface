d9fb8e57833142f7452cb6ad4e508efd675449a1
SullFurix <sull.furix@gmail.com>
2022-01-11 22:33:01 +0100

    app: move index to app

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import { Web3ReactProvider } from "@web3-react/core";
+
+import { ethers } from "ethers";
+
+
+import UserLayout from "./layouts/User.js";
+import Layout from "./layouts/Layout.js";
+
+function getLibrary(provider) {
+  const library = new ethers.providers.Web3Provider(provider);
+  library.pollingInterval = 8000;
+  return library;
+}
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Web3ReactProvider getLibrary={getLibrary}>
+          <Switch>
+            <Route path={`/user`} component={UserLayout} />
+            <Route path={`/`} component={Layout} />
+            <Redirect from={`/`} to="home" />
+          </Switch>
+      </Web3ReactProvider>
+    </HashRouter>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import App from "./App.js";
 
-import { Web3ReactProvider } from "@web3-react/core";
-
-import { ethers } from "ethers";
-
-import UserLayout from "./layouts/User.js";
-import Layout from "./layouts/Layout.js";
-
-function getLibrary(provider) {
-  const library = new ethers.providers.Web3Provider(provider);
-  library.pollingInterval = 8000;
-  return library;
-}
-
-ReactDOM.render(
-  <HashRouter>
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Switch>
-        <Route path={`/user`} component={UserLayout} />
-        <Route path={`/`} component={Layout} />
-        <Redirect from={`/`} to="home" />
-      </Switch>
-    </Web3ReactProvider>
-  </HashRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
